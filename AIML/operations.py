dcd08f536e7a828316371b68348d6dcf06667681
rakshith-p <rakshith.padmanabha@gmail.com>
2016-10-25 11:13:22 -0700

    Inclusion of Relearn Module in AIMLPraser-Operations and update of AIML script

--- AIML/operations.py
+++ AIML/operations.py
@@ -1,7 +1,7 @@
 import aiml
 import os
 
-
+#<-- version 1.2 with Added Relearn Mechanism 
 
 class AIMLParser(object):
 	
@@ -9,12 +9,18 @@ class AIMLParser(object):
 		self.kernel = aiml.Kernel()
 		if os.path.isfile("bot_brain.brn"):
 			self.kernel.bootstrap(brainFile = "bot_brain.brn")
+			
 		else:
-			self.kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
-			self.kernel.saveBrain("bot_brain.brn")
+			self.reLearn()
 
 	def getAction(self,MLInput):
-		self.msg=MLInput
+		self.application=MLInput[0]
+		self.msg=MLInput[1]
+		self.initial = self.kernel.respond(self.application)
 		self.bot_response = self.kernel.respond(self.msg)
 		return self.bot_response
+	
+	def reLearn(self):
+		self.kernel.bootstrap(brainFile = "bot_brain.brn",learnFiles = "std-startup.xml", commands = "load aiml b")
+		self.kernel.saveBrain("bot_brain.brn")
 		
